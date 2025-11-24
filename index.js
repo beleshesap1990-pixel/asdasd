07258103b605b6c171d5bf04e45b4412f53bdaa3
beleshesap1990-pixel <beleshesap1990@gmail.com>
2025-11-24 16:23:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 import express from "express";
-import request from "request";
+import fetch from "node-fetch";
 import cors from "cors";
 
 const app = express();
 app.use(cors());
 
-app.use("/", (req, res) => {
-  const url = "https://play-cs.com" + req.url;
-  req.pipe(request(url)).pipe(res);
+app.use("/", async (req, res) => {
+  const target = "https://play-cs.com" + req.url;
+
+  try {
+    const response = await fetch(target);
+    const data = await response.text();
+
+    res.send(data);
+  } catch (err) {
+    res.status(500).send("Proxy error: " + err.toString());
+  }
 });
 
-app.listen(3000, () => {
-  console.log("Proxy aktif!");
-});
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log("Proxy running on " + port));
+
